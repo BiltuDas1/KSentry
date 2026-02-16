dd4b60100f1654a113a5eaed18a91fd216f2cfac
BiltuDas1 <billionto@gmail.com>
2026-02-16 14:20:30 +0530

    fix: Code reviewed user information is not removing from redis

--- services/closed_pull.py
+++ services/closed_pull.py
@@ -1,6 +1,6 @@
 from models import PullRequesPayload
 from core import settings
-from utils import collaborators
+from utils import collaborators, jwt
 
 
 async def pull_request_closed(payload: PullRequesPayload):
@@ -14,6 +14,27 @@ async def pull_request_closed(payload: PullRequesPayload):
   for reviewers in payload.pull_request.requested_reviewers:
     await collaborators.remove_pr(
       username=reviewers.login,
+      repo=payload.repository.full_name,
+      pr_number=payload.number,
+    )
+
+  # Get the actual reviewers and remove them from the pr
+  token: str = await jwt.get_installation_token(payload.installation.id)
+  url = f"https://api.github.com/repos/{payload.repository.full_name}/pulls/{payload.number}/reviews"
+  headers = {
+    "Authorization": f"Bearer {token}",
+    "Accept": "application/vnd.github+json",
+  }
+  response = await settings.HTTPX.get(url=url, headers=headers)
+  if response.status_code != 200:
+    return
+
+  for reviewers in response.json():
+    if reviewers["user"]["type"].lower() != "user":
+      continue
+
+    await collaborators.remove_pr(
+      username=reviewers["user"]["login"],
       repo=payload.repository.full_name,
       pr_number=payload.number,
     )
